0af80c94c63fda0e2325193fddffb11f25b7d6e0
lgq1205 <422895018@qq.com>
2019-04-14 12:59:16 +0800

    autoinstall

---
+++ autoinstall.sh
@@ -0,0 +1,29 @@
+#!/bin/sh
+
+
+#####   install brew
+sh -c "$(curl -fsSL https://raw.githubusercontent.com/Linuxbrew/install/master/install.sh)"
+test -d ~/.linuxbrew && eval $(~/.linuxbrew/bin/brew shellenv)
+test -d /home/linuxbrew/.linuxbrew && eval $(/home/linuxbrew/.linuxbrew/bin/brew shellenv)
+test -r ~/.bash_profile && echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.bash_profile
+echo "eval \$($(brew --prefix)/bin/brew shellenv)" >>~/.profile
+
+sudo apt-get install build-essential curl file git
+
+git clone https://github.com/Homebrew/brew ~/.linuxbrew/Homebrew
+mkdir ~/.linuxbrew/bin
+ln -s ../Homebrew/bin/brew ~/.linuxbrew/bin
+eval $(~/.linuxbrew/bin/brew shellenv)
+
+####    install ctags
+brew install ctags
+
+####    environment
+git clone https://github.com/lgq1205/environment/blob/master/.vimrc ~/.vimrc
+git clone https://github.com/lgq1205/environment/blob/master/.tmux.conf ~/.tmux.conf
+
+
+####    oh-my-zsh
+$ sh -c "$(curl -fsSL https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh)"
+
+
